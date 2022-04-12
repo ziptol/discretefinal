ff36bf9fda36695234a646826f7bbd4901fc922f
Zack Perry <60787475+ziptol@users.noreply.github.com>
2022-04-12 13:47:27 -0400

    Added main, got matplotlib working

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-def plotData():
+print("\nWould you like to:")
+print("1. Graph the number of digits it takes to represent decimal number 'n' in base 2-36")
+print("2. Graph the number of digits it takes to represent decimal 1-100000 in base 'n'")
+print("Choice: ")
+userChoice = int(input())
 
-    return 0
+
+if(userChoice == 1):
+    print("1")
+    testData = [1,5,10,123,50,12]
+    plt.plot(testData)
+    plt.show()
+elif(userChoice == 2):
+    print("2")
